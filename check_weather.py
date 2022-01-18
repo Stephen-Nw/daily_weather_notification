b3ccb13225b7ebc838aab653a09b22d4c0bb23ce
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-01-18 17:48:21 -0600

    wrap temperature retrieval logic in daily temp fxn

--- check_weather.py
+++ check_weather.py
@@ -18,17 +18,23 @@ weather_parameters = {
 
 }
 
-response = requests.get(WEATHER_ENDPOINT, params=weather_parameters)
-response.raise_for_status()
-weather_data = response.json()
-# print(weather_data)
-# print("=========================================================")
-current_temperature = weather_data['current']['temp']
-feels_like = weather_data['current']['feels_like']
-# print(current_temperature)
-# print(feels_like)
-max_temperature = weather_data['daily'][0]['temp']['max']
-# print(max_temperature)
-min_temperature = weather_data['daily'][0]['temp']['min']
-# print(min_temperature)
+
+def daily_temperature():
+    """Obtain temperature readings for current day"""
+    response = requests.get(WEATHER_ENDPOINT, params=weather_parameters)
+    response.raise_for_status()
+    weather_data = response.json()
+    # print(weather_data)
+    # print("=========================================================")
+    current_temperature = weather_data['current']['temp']
+    feels_like = weather_data['current']['feels_like']
+    # print(current_temperature)
+    # print(feels_like)
+    max_temperature = weather_data['daily'][0]['temp']['max']
+    # print(max_temperature)
+    min_temperature = weather_data['daily'][0]['temp']['min']
+    # print(min_temperature)
+    return current_temperature, feels_like, max_temperature, min_temperature
+
+
 
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import requests
+from check_weather import daily_temperature
+
+dt = daily_temperature()
+print(dt)
+print(dt[0])
+
