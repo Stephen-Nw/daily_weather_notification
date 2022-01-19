d84f052b54f5bb7fe0b86acaa47d8399e754bf01
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-01-19 07:39:22 -0600

    update SMS message to user

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os
+from twilio.rest import Client
 from check_weather import daily_temperature
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
 dt = daily_temperature()
 print(dt)
 print(dt[0])
 
+# return current_temperature, feels_like, max_temperature, min_temperature
+
+message = client.messages \
+                .create(
+                     body=f"Good morning, Beautiful. Here is your daily weather forecast:"
+                          f"\nCurrent temp: {dt[0]}℉"
+                          f"\nFeels like: {dt[1]}℉"
+                          f"\nMax temp: {dt[2]}℉"
+                          f"\nMin temp: {dt[3]}℉",
+                     from_='+16205914475',
+                     to='+19188600282'
+                 )
